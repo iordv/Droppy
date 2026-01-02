56638786432f9ae2f6b078348584b4293ffcf8ac
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-02 18:57:33 +0100

    feat: Update formula URL and homepage to correct GitHub repository.

--- droppy.rb
+++ droppy.rb
@@ -2,11 +2,10 @@ cask "droppy" do
   version "1.0"
   sha256 "b189900bf39f24b5ca259fb9fdd0260fa14972b42584224e0e0e6936e422079a"
 
-  # Replace YOUR_GITHUB_USERNAME and YOUR_REPO_NAME with your actual GitHub details
-  url "https://github.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME/releases/download/v#{version}/Droppy.dmg"
+  url "https://raw.githubusercontent.com/iordv/Droppy/main/Droppy.dmg"
   name "Droppy"
   desc "Drag and drop file shelf for macOS"
-  homepage "https://github.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME"
+  homepage "https://github.com/iordv/Droppy"
 
   app "Droppy.app"
 
