dcc45a104daf905609b006708e2b982d7248208c
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-23 01:26:51 +0100

    Release v9.3.0

--- docs/assets/js/version.js
+++ docs/assets/js/version.js
@@ -3,8 +3,8 @@
 
 const DROPPY_CONFIG = {
     // Fallback values if GitHub API fails
-    version: '9.2.0',
-    dmgUrl: 'https://github.com/iordv/Droppy/releases/latest/download/Droppy-9.2.0.dmg',
+    version: '9.3.0',
+    dmgUrl: 'https://github.com/iordv/Droppy/releases/latest/download/Droppy-9.3.0.dmg',
     releasesUrl: 'https://github.com/iordv/Droppy/releases/latest',
 
     // GitHub API endpoint
